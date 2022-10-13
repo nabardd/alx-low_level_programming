769f380db6a3c5d4d35d60157283c3add4f5479b
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-13 11:19:53 +0100

    0x00-hello_world/Add advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function to print string to stderr
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar,");
+	fprintf(stderr, " 2015-10-19\n");
+
+	return (1);
+}
