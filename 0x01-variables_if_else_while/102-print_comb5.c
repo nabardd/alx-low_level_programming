837640f477b17b331e2cb1ae89a36ddfdaf3f658
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-16 15:44:41 +0100

    Adjust 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,16 +2,7 @@
 /**
  * main -  function that prints all possible 
  * combinations of two two-digit numbers.
- * Description: The numbers should range from 0 to 99,
- * The two numbers should be separated by a space,
- * All numbers should be printed with two digits.
- * 1 should be printed as 01
- * The combination of numbers must be separated by comma,
- * followed by a space, 
- * The combinations of numbers should be printed in ascending order,
- * 00 01 and 01 00 are considered as the 
- * same combination of the numbers 0 and 1
- * 
+ *
  * Return: 0, different combinations. 
 */
 
