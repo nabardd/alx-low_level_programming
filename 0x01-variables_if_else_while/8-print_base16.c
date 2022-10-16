8be6df7271c9fffa29c54831721bcd88aecb9ba9
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-16 14:58:30 +0100

    Adjust 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,26 +2,26 @@
 
 /**
  * main - function to print all base 16 numbers
- * 
- * Returns: 0, prints all base 16 numbers
+ *
+ * Return: 0, prints all base 16 numbers
 */
 
 int main(void)
 {
-  int n = 48;
-  int alpha = 97;
+	int n = 48;
+	int alpha = 97;
 
-  while (n <= 57)
-  {
-    putchar(n);
-    n++;
-  }
-  while (alpha <= 102)
-  {
-    putchar(alpha);
-    alpha++;
-  }
-  putchar("\n");
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (alpha <= 102)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
