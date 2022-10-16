4223d347a7486c867169a5f84acef17a47145fae
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-16 14:50:02 +0100

    Adjust 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,21 @@
 
 /**
  * main - function to print digits from 0 to 9
- * 
- * Returns: 0, prints numbers 0 - 9
+ *
+ * Return: 0, prints numbers 0 - 9
 */
 
 int main(void)
 {
-  int n = 48;
+	int n = 48;
 
-  while (n <= 57)
-  {
-    putchar(n);
-    n++;
-  }
-  putchar("\n");
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar("\n");
+
+	return (0);
 }
