ea08071855203833f22ee1af0d34b335631dca1e
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-16 14:26:38 +0100

    Adjust 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,19 +3,19 @@
 /**
  * main - function that prints out small letter alphabets
  *
- * Returns: 0 if successful, and prints out lowercase alphabets
+ * Return: 0 if successful, and prints out lowercase alphabets
 */
 
 int main(void)
 {
 	int n = 97;
-	
+
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
