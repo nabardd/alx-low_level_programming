cc7f454f9cbe87bc4c0c7df30c39e6ade81f810b
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-16 15:15:58 +0100

    Adjust 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,37 +4,39 @@
  * main - function that prints all possible
  *        different combinations of three digits.
  *        Numbers must be separated by ',', followed by a space
- * 
- * Returns: 0, different combinations.
- *        
+ *
+ * Return: 0, different combinations.
 */
 
 int main(void)
 {
-  int i, first, middle, last;
+	int i, first, middle, last;
 
-  i = 0;
+	i = 0;
 
-  while (i < 1000)
-  {
-    first = i / 1000;
-    middle = (i / 10) % 10;
-    last = i % 10;
+	while (i < 1000)
+	{
+		first = i / 1000;
+		middle = (i / 10) % 10;
+		last = i % 10;
 
-    if (first < middle && middle < last)
-    {
-      putchar(first + '0');
-      putchar(middle + '0');
-      putchar(last + '0');
+		if (first < middle && middle < last)
+		{
+			putchar(first + '0');
+			putchar(middle + '0');
+			putchar(last + '0');
 
-      if (i < 1000)
-      {
-        putchar(44);
-        putchar(32);
-      }
-    }
-  }
-  putchar("\n");
+			if (i < 1000)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
 
-  return (0);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
