8494178769fc0352953fd572b141552a8ea77d3b
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-19 00:38:44 +0100

    Corrected to Betty  style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,22 +2,24 @@
 
 /**
  * _isalpha - checks for alphabetic character, uppercase or lowercase.
- * @c - integer representation of the the character to be checked.
+ * @c: integer representation of the the character to be checked.
  *
  * Return: 1 if c is a letter, 0 otherwise.
  */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 
-	} else if (c >= 65 && c <= 90)
+	}
+	else if (c >= 65 && c <= 90)
 	{
 		return (1);
 
-	} else
+	}
+	else
 	{
 		return (0);
 	}
