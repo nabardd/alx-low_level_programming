ce4f12e9bd579694b5db843f2a114d4435abcee8
nabardd <ojebiyidaviddaniel@gmail.com>
2022-10-25 20:51:00 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - takes a function to an int as parameter and
  *		updates the value it points to to 98.
- * @*n: pointer to the int.
+ * @n: pointer to the int.
  *
  * Return: Nothing.
  */
