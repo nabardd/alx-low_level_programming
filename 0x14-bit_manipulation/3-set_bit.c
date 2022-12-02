acdef49abda9e8b5844c1708e64c60241deec4b7
nabardd <ojebiyidaviddaniel@gmail.com>
2022-12-02 17:56:37 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 /**
  * set_bit - Function that sets the value of a bit to 1 at a given index
  * @n: number to be checked through
- * index: index, starting from 0 of the bit you want to set
+ * @index: index, starting from 0 of the bit you want to set
  *
  * Return: 1 if worked, -1 otherwise
  */
