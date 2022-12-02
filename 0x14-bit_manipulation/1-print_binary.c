37393427e7630f7a67d361b3eaedc5069b992043
nabardd <ojebiyidaviddaniel@gmail.com>
2022-12-02 16:58:39 +0100

    Add 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - Function that prints the binary representation
+ * of a number
+ * @n: number to print out binary classification
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	while (n != 0)
+	{
+		print_binary(n >> 1);
+	}
+
+	_putchar((n & 0x01) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,2 +1,3 @@
 #include <stddef.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
