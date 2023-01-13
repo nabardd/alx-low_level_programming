7023a4f0f0ab9d83c924d08247c24829fcb5649f
nabardd <ojebiyidaviddaniel@gmail.com>
2023-01-13 04:30:25 +0100

    Modify lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef DLL
+#define DLL
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,3 +25,5 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+#endif
