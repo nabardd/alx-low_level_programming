e915ece9c08707c68c3773023c83cc688f292291
nabardd <ojebiyidaviddaniel@gmail.com>
2023-01-16 20:52:10 +0100

    Add Advanced tasks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - adds two integer
+ * @a: first integer to be added
+ * @b: second integer to be added
+ *
+ * Return: sum of numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts b from a
+ * @a: first value
+ * @b: second value
+ *
+ * Return: difference of numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides integer a by integer b
+ * @a: Numerator
+ * @b: denominator
+ *
+ * Return: result from division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder of division
+ * @a: Numerator
+ * @b: Denominator
+ *
+ * Return: remainder from division
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
